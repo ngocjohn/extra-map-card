23a0f7418025e5ed9109ebd647b089d1dfd0ae5b
Viet Ngoc <ngocjohn@me.com>
2025-03-28 19:56:35 +0100

    fix: update Rollup configuration to use @rollup/plugin-commonjs

--- rollup.config.helper.mjs
+++ rollup.config.helper.mjs
@@ -1,5 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
